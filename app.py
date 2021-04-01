426b1c79d652552282ce756b6cb5bfd66ed80ca8
zaid wawi <76659815+zaidwawi@users.noreply.github.com>
2021-04-01 13:58:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import (Flask, request, abort, jsonify)
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db, Actor, Movie, rollback
@@ -7,273 +7,281 @@ from auth import requires_auth, AuthError
 
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  CORS(app)
+
+    # create and configure the app
+
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+# create the actors action here 
+
+    @app.route('/actors', methods=['GET']) # set a get request for actors
+    @requires_auth('get:actors')
+    def get_actors(jwk):
+        actor = Actor.query.all() # get all actors detail 
+        return jsonify({
+            "success": True,
+            "actors": [actors.format() for actors in actor]
+        }), 200
+
+    @app.route('/actors/<int:actor_id>', methods=['GET']) # get the actorst by_id
+    @requires_auth('get:actors')
+    def get_actor(jwk, actor_id):
+        actor = Actor.query.get(actor_id) # I get the code here by id 
+        if actor is None: # if there is no actor I will abort 404 or (not found)
+            abort(404)
+        return jsonify({
+            "success": True,
+            "actor": actor.format()
+        }), 200
+
+    @app.route('/actors', methods=['POST']) # post an actor
+    @requires_auth('post:actors')
+    def post_actors(jwk):
+        body = request.get_json()
+
+        add_actor = Actor() 
+
+        name = body.get('name') # I get the name
+        age = body.get('age') # I get the age 
+        gender = body.get('gender') # I get the gender 
+        if (name is None) or (age is None) or (gender is None): # if there is no age-name-gender I want to give 422 error 
+            abort(422)
+        try:
+            if name is not None: # if the name is not None (thats mean if the name exist)
+                add_actor.name = name
+            if age is not None: 
+                add_actor.age = age
+            if gender is not None:
+                add_actor.age = age
+
+            add_actor.insert() # I insert the data to the database 
+        except Exception:
+            abort(500)
+        return jsonify({
+            "success": True,
+            "created_actor": add_actor.format()
+        }), 200
+
+    @app.route('/actors/<int:actor_id>', methods=['PATCH']) # patch the data or (update it)
+    @requires_auth('patch:actors')
+    def edit_actors(jwk, actor_id):
+        body = request.get_json() 
+        
+        actor = Actor.query.get(actor_id)
+
+        if actor is None: # if there is no actor I want to raise 404 error (not found)
+            abort(404)
+
+        new_name = body.get('name')
+        new_age = body.get('age') # get the age and the name as will as the gender 
+        new_gender = body.get('gender')
+
+        if (new_name is None) or (new_age is None) or (new_gender is None):
+            abort(422)
+
+        try:
+            if new_name is not None:
+                actor.name = new_name
+            if new_age is not None:
+                actor.age = new_age # if the data i exist => I will add it to thhe database
+            if new_gender is not None:
+                actor.gender = new_gender
+
+            actor.update()
+        except:
+            rollback() #d if there is something wrong I want to rollback I set this func in the models.py 
+            abort(422)
+
+        return jsonify({
+            "success": True,
+            "patched_actor": actor.format()
+        }), 200
+
+    @app.route('/actors/<int:actor_id>', methods=['DELETE'])
+    @requires_auth('delete:actors')
+    def delete_actors(jwk, actor_id):
+        actor = Actor.query.get(actor_id) # get the actor by id 
+        if actor is None:
+            abort(404)
+
+        try:
+            actor.delete() # if there is nothing wrong I will delete it 
+        except Exception:
+            rollback() # but if there is an error I want to rollback and raise 500 error
+            abort(500)
+
+        return jsonify({
+            "success": True,
+            "deleted_actor": actor.format()
+        }), 200
 
 
-# here I want to setup the actors action 
-  @app.route('/')
-  def home():
-    return 'welcome to my website'
-
-  @app.route('/actors', methods=['GET'])
-  @requires_auth('get:actors')
-  def get_actor(jwk):
-    actors = Actor.query.all()
-    Actor = [actor.format() for actor in actors]
-    return jsonify({
-      "success": True,
-      "actors": [actor.format() for actor in actors]
-    }), 200
-
-
-  @app.route('/actors', methods=['POST'])
-  @requires_auth('post:actors')
-  def post_actor(jwk):
-    body = request.get_json()
-    name = body.get('name')
-    age = body.get('age')
-    gender = body.get('gender')
-
-    if ((age is None) or (name is None) or (gender is None)):
-      abort(400)
-    
-    try:
-      the_actor = Actor(name=name, age=age, gender=gender)
-      the_actor.insert()
-
-    except Exception:
-      abort(500)
-  
-    return jsonify({
-      "success": True,
-      "created": the_actor.id
-    }), 200
-
-  @app.route('/actors/<int:id>', methods=['GET'])
-  @requires_auth('get:actors')
-  def get_actor_by_id(jwk, id):
-    actors = Actor.query.get(id)
-    if actors is None:
-      abort(404)
-    return jsonify({
-      "success": True,
-      "actors": actors.format()
-    }), 200
-  
-
-
-
-  @app.route('/actors/<int:id>', methods = ['PATCH'])
-  @requires_auth('patch:actor')
-  def patch_the_actor(jwk, id):
-    body = request.get_json()
-    actor = Actor.query.get(id)
-
-    if actor is None:
-      abort(404)
-
-    age = body.get('age')
-    name = body.get('name')
-    gender = body.get('gender')
-
-    if ((age is None) or (name is None) or (gender is None)):
-      abort(422)
-    
-    try:
-      if age is not None:
-        actor.age = age
-      if name is not None:
-        actor.name = name
-      if gender is not None:
-        actor.gender = gender
-
-      actor.update()
-    except Exception:
-      rollback()
-      abort(422)
-
-    return jsonify({
-      "success": True,
-      "actor": [actor.format()]
-    }), 200
-  
-
-
-  @app.route('/actors/<int:id>', methods=['DELETE'])
-  @requires_auth('delete:actor')
-  def delete_actor(jwk, id):
-    actor = Actor.query.get(id)
-
-    if actor is None:
-      abort(404)
-    
-    try:
-      actor.delete()
-    except Exception:
-      rollback()
-      abort(500)
-
-    return jsonify({
-      "success": True,
-      "movies": [actor.format()]
-    }), 200
-
-  
-  # here I want to setup the movie actions 
-
-
-  @app.route('/movies', methods=['GET'])
-  @requires_auth('get:movies')
-  def get_movie(jwk):
-    movies = Movie.query.all()
-    return jsonify({
-      "success": True,
-      "movies": [movie.format() for movie in movies]
-
-    }), 200
-  
-
-  @app.route('/movies/<int:id>', methods=['GET'])
-  @requires_auth('get:movies')
-  def get_movie_by_id(jwk, id):
-    movie = Movie.query.get(id)
-
-    if movie is None:
-      abort(404)
-    return jsonify({
-      "success": True,
-      "movie": movie.format()
-    }), 200
-  
-
-  
-  @app.route('/movies', methods=['POST'])
-  @requires_auth('post:movie')
-  def post_movie(jwk):
-    body = request.get_json()
-
-    title = body.get('title')
-    release_date = body.get('release_date')
-
-    if (title is None) or (release_date is None):
-      abort(400)
-
-    try:
-      the_movie = Movie()
-      the_movie.title = title
-      the_movie.release_date = release_date
-      the_movie.insert()
-
-      return jsonify({
-        "success": True,
-        "created_movie": the_movie.format()}), 200
-    except Exception:
-      abort(500)
-
-  @app.route('/movies/<int:id>', methods=['PATCH'])
-  @requires_auth('patch:movie')
-  def patch_movie(jwk, id):
-    body = request.get_json()
-    movie = Movie.query.get(id)
-
-    if movie is None:
-      abort(404)
-    
-    try:
-      title = body['title']
-      release_date = body['release_date']
-
-      if title is not None:
-        movie.title = title
-      if release_date is not None:
-        movie.release_date = release_date
-      movie.update()
-
-    except Exception:
-      abort(500)
-    
-    return jsonify({
-        "success": True,
-        "movie": movie.format()
-      }), 200
-
-  @app.route('/movies/<int:id>', methods=['DELETE'])
-  @requires_auth('delete:movie')
-  def delete_movie(jwk, id):
-    movie = Movie.query.get(id)
-
-    if movie is None:
-      abort(404)
-
-    try:
-      movie.delete()
-    except Exception:
-      rollback()
-      abort(500)
-    return jsonify({
-      "success": True,
-      "deleted": movie.format()
-      }), 200
+# here I will setup the movie (get , patch , post and delete) requests
 
 
 
-      
+    @app.route('/movies', methods=['GET']) # get the movie 
+    @requires_auth('get:movies')
+    def get_movies(jwk):
+        movies = Movie.query.all()
 
+        return jsonify({
+            "success": True,
+            "movies": [movie.format() for movie in movies]
+        })
+
+    @app.route('/movies/<int:movie_id>', methods=['GET'])
+    @requires_auth('get:movies')
+    def get_movie(jwk, movie_id):
+        movie = Movie.query.get(movie_id) # get the movie by id 
+        if movie is None:
+            abort(404) # if the movie does not exist I want to raise 404 error 
+        return jsonify({
+            "success": True,
+            "movie": [movie.format()]
+        })
+
+    @app.route('/movies', methods=['POST']) # create movie 
+    @requires_auth('post:movies')
+    def post_movies(jwk):
+        body = request.get_json()
+
+        new_title = body.get('title') 
+        new_release_date = body.get('release_date')
+
+        if (new_title is None) or (new_release_date) is None: # if the new title and new release date are empty I want to raise 400 error 
+            abort(400)
+
+        try:
+            if (new_title is not None) and (new_release_date is not None): # if the new title and release date are not empty I want to add them
+                movie = Movie(
+                    title=new_title,
+                    release_date=new_release_date
+                )
+            movie.insert() # insert the data
+
+        except Exception:
+            abort(500)
+
+        return jsonify({
+            "success": True,
+            "created_movie": movie.id
+        })
+
+    @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+    @requires_auth('patch:movies')
+    def edit_movies(jwk, movie_id):
+
+        movie = Movie.query.get(movie_id)
+
+        if movie is None:
+            abort(404)
+
+        body = request.get_json()
+
+        title = body.get("title")
+        release_date = body.get("release_date")
+
+        if (title is None) or (release_date is None):
+            abort(422)
+
+        try:
+            if title is not None:
+                movie.title = title
+            if release_date is not None:
+                movie.relaese_date = release_date
+            movie.update()
+
+        except Exception:
+            abort(422)
+
+
+        return jsonify({
+            "success": True,
+            "patched_movie": movie.format()
+        }), 200
+
+    @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+    @requires_auth('delete:movies')
+    def delete_movies(jwk, movie_id):
+        movie = Movie.query.get(movie_id)
+        if movie is None:
+            abort(404)
+
+        try:
+            movie.delete()
+        except Exception:
+            rollback()
+            abort(422)
+
+
+        return jsonify({
+            "success": True,
+            "deleted_movie": movie.format()
+        }), 200
+
+
+
+# handle error =====================================================================
 
 
     @app.errorhandler(400)
     def bad_request(error):
         return jsonify({
-          "success": False,
-          "error": 400,
-          "message": 'Bad request'
+            "success": False,
+            "error": 400,
+            "message": 'Bad request'
         }), 400
-
-    @app.errorhandler(404)
-    def not_found_error(error):
-        return jsonify({
-          "success": False,
-          "error": 404,
-          "message": 'Resource not found '
-        }), 404
-
-    @app.errorhandler(422)
-    def unprocessable(error):
-        return jsonify({
-          "success": False,
-          "error": 422,
-          "message": 'unprocessable'
-        }), 422
 
     @app.errorhandler(401)
     def unauthorized(error):
         return jsonify({
-          "success": False,
-          "error": 401,
-          "message": "Unauthorized"
+            "success":False,
+            "error": 401,
+            "message": "Unauthorized"
         }), 401
 
     @app.errorhandler(403)
     def forbidden(error):
         return jsonify({
-          "success": False,
-          "error": 403,
-          "message": "Forbidden"
+            "success": False,
+            "error": 403,
+            "message": "Forbidden"
         }), 403
+
+
+    @app.errorhandler(404)
+    def resource_not_found_error(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": 'unprocessable'
+        }), 422
+
+
+
     @app.errorhandler(500)
     def internal_server_error(error):
         return jsonify({
-          "success": False,
-          "error": 500,
-          "message": "Internal server error"
+            "success": False,
+            "error": 500,
+            "message": "Internal server error"
         }), 500
 
-  return app
+    return app
+
 
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run()
